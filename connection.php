554bcd26cf66eb5f936e93fdb4416addaf19ec0d
Thomas Nathan Feeney <31934348+nathanfeeney@users.noreply.github.com>
2021-02-01 21:26:08 +0000

    testing only

--- connection.php
+++ connection.php
@@ -3,16 +3,12 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dissertaion";
+$dbname = "dissertation";
 
 //Create connection
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+
 
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include_once 'connection.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,5 +9,25 @@
 </head>
 <body>
     <?php echo '<p>Hello World</p>'; ?>
+    
+    <?php
+    $sql = "SELECT * FROM users;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    
+    if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row['store_name'] . "<br>";
+            echo $row['qr_code']. "<br>";
+            echo $row['location']. "<br>";
+            echo $row['password']. "<br>";
+            echo $row['menu']. "<br>";
+            echo $row['deals']. "<br>";
+            echo $row['events']. "<br>";
+            
+        }
+    }
+    ?>
+    
 </body>
 </html>
