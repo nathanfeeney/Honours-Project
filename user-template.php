e0bd7e3509282aa3883e37a2437086289890b68b
Thomas Nathan Feeney <tnfeeney@hotmail.com>
2021-02-19 21:21:22 +0000

    passing id works

--- user-template.php
+++ user-template.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 $sq = "SELECT * FROM organisations WHERE orgID='$id'";
 
 echo "<b> <center>Database Output</center> </b> <br> <br>";
-
+echo '<a href="user-template.php?id=1"> 2 </a>';
 if ($result = $conn->query($sq)) {
 
     while ($row = $result->fetch_assoc()) {
